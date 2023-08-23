e8fe5fa8bed3349746101c66e332b9fccc66eef7
Tage <05jota@skola.engelholm.se>
2023-08-23 12:37:43 +0200

    ja

--- www/public/index.php
+++ www/public/index.php
@@ -1,2 +1,19 @@
-<h1>Webbservern fungerar!</h1>
-<h1>hejsan nu ändrar jag hemsidan<h1>
+<!doctype html>
+<html>
+<head lang="sv"></head>
+<body>
+<h1>Matematik-test</h1>
+<form action="evaluate.php" method="post">
+   <fieldset>
+       <legend>Frågor</legend>
+       <label>4 + 5 = </label>
+       <input type="text" name="q1">
+       <br>
+       <label>3 x 5 = </label>
+       <input type="text" name="q2">
+       <br>
+       <input type="submit" value="Rätta">
+   </fieldset>
+</form>
+</body>
+</html>
